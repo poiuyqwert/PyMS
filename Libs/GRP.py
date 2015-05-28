poiuyqwert/PyMS

commit 7b8421bbc899989ac454a92e883ba957ee63fc4c

- Render some widgets with dlg.grp and tile.grp assets - Tightened resizing area

Libs/GRP.py
@@ -35,11 +35,45 @@ def rle_outline(pal, index, ally_status=OUTLINE_SELF):
 			rgb = (220,220,60)
 	return (rgb[0],rgb[1],rgb[2], 255)
 
-# def merge(l):
-	# z = []
-	# for r in l:
-		# z.extend(r)
-	# return z
+def image_bounds(image, transindex=0):
+	bounds = [-1,-1,-1,-1]
+	for y,yd in enumerate(d):
+		if yd.count(transindex) != g.width:
+			if bounds[1] == -1:
+				bounds[1] = y
+			bounds[3] = y
+			line = yd
+			for x,xd in enumerate(line):
+				if xd != transindex:
+					if bounds[0] == -1 or x < bounds[0]:
+						bounds[0] = x
+					if x > bounds[2]:
+						bounds[2] = x
+	return bounds
+
+# transindex=None for no transparency
+def image_to_pil(image, palette, transindex=0, image_bounds=None, flipHor=False, draw_function=rle_normal, draw_info=None):
+	if image_bounds:
+		x_min,y_min,x_max,y_max = image_bounds
+		image = list(line[x_min:x_max+1] for line in image[y_min:y_max+1])
+	width = len(image[0])
+	height = len(image)
+	i = PILImage.new('RGBA', (width,height))
+	data = []
+	pal = map(lambda i: draw_function(palette,i,draw_info), xrange(len(palette)))
+	if transindex != None:
+		pal[transindex] = (0,0,0,0)
+	if flipHor:
+		image = map(reversed, image)
+	data = itertools.chain.from_iterable(image)
+	data = map(pal.__getitem__, data)
+	i.putdata(data)
+	return i
+
+def image_to_tk(image, palette, transindex=0, image_bounds=None, flipHor=False, draw_function=rle_normal, draw_info=None):
+	pil = image_to_pil(image, palette, transindex, image_bounds, flipHor, draw_function, draw_info)
+	return ImageTk.PhotoImage(pil)
+
 def frame_to_photo(p, g, f=None, buffered=False, size=True, trans=True, transindex=0, flipHor=False, draw_function=None, draw_info=None):
 	if f != None:
 		if buffered:
@@ -96,10 +130,11 @@ class CacheGRP:
 		self.palette = palette
 		self.imagebuffer = []
 		self.images = {}
+		self.image_frames = {}
 		self.databuffer = ''
 		self.uncompressed = None
 
-	def load_file(self, file, palette=None, restrict=None):
+	def load_file(self, file, palette=None, restrict=None, uncompressed=None):
 		try:
 			if isstr(file):
 				f = open(file,'rb')
@@ -132,13 +167,16 @@ class CacheGRP:
 		self.imagebuffer = images
 		self.images = {}
 		self.databuffer = data
+		self.uncompressed = uncompressed
 
 	def __getitem__(self, frame):
 		if not frame in self.images:
 			image = []
 			xoffset, yoffset, linewidth, lines, offsets = self.imagebuffer[frame]
-			if xoffset + linewidth > self.width or yoffset + lines > self.height:
-				raise
+			if xoffset + linewidth > width:
+				linewidth = width - xoffset
+			if yoffset + lines > height:
+				lines = height - yoffset
 			image.extend([[0] * self.width for _ in range(yoffset)])
 			if not self.uncompressed:
 				try:
@@ -171,13 +209,15 @@ class CacheGRP:
 						linedata = []
 						if xoffset > 0:
 							linedata = [0] * xoffset
-						linedata.extend([ord(index) for index in data[offset:offset+linewidth]])
-						image.append(linedata + [0] * (width-linewidth-xoffset))
+						linedata.extend([ord(index) for index in self.databuffer[offset:offset+linewidth]])
+						image.append(linedata + [0] * (self.width-linewidth-xoffset))
 				except:
+					raise
 					raise PyMSError('Decompile','Could not decompile frame %s, GRP could be corrupt.' % frame)
 			if len(image) < self.height:
 				image.extend([[0] * self.width for _ in range(self.height - len(image))])
 			self.images[frame] = image[:self.height]
+			self.image_frames[frame] = (xoffset,yoffset,linewidth,lines)
 		return self.images[frame]
 
 class GRP:
@@ -187,6 +227,7 @@ class GRP:
 		self.height = 0
 		self.palette = palette
 		self.images = []
+		self.images_bounds = []
 		self.uncompressed = uncompressed
 		self.transindex = transindex
 
@@ -208,6 +249,7 @@ class GRP:
 			while retries:
 				retries -= 1
 				images = []
+				images_bounds = []
 				for frame in range(frames):
 					image = []
 					xoffset, yoffset, linewidth, lines, framedata = struct.unpack('<4BL', data[6+8*frame:14+8*frame])
@@ -253,6 +295,7 @@ class GRP:
 					if len(image) < height:
 						image.extend([[transindex] * width for _ in range(height - len(image))])
 					images.append(image[:height])
+					images_bounds.append((xoffset,yoffset,xoffset+linewidth-1,yoffset+lines-1))
 		except:
 			raise PyMSError('Load',"Unsupported GRP file '%s', could possibly be corrupt" % file,exception=sys.exc_info())
 		self.frames = frames
@@ -262,6 +305,7 @@ class GRP:
 		if palette:
 			self.palette = list(palette)
 		self.images = images
+		self.images_bounds = images_bounds
 		self.transindex = transindex
 
 	def load_data(self, image, palette=None, transindex=0):
@@ -271,6 +315,7 @@ class GRP:
 		if palette:
 			self.palette = list(palette)
 		self.images = [list(image)]
+		self.images_bounds = [image_bounds(image, transindex)]
 		self.transindex = transindex
 
 	def save_file(self, file, uncompressed=None):
@@ -288,18 +333,19 @@ class GRP:
 		offset = 6 + 8 * self.frames
 		frame_history = {}
 		for z,frame in enumerate(self.images):
-			x_min, x_max, y_min, y_max = (-1,)*4
-			for y,line in enumerate(frame):
-				if line != [self.transindex] * len(line):
-					if y_min == -1:
-						y_min = y
-					y_max = y + 1
-					for x,index in enumerate(line):
-						if index != self.transindex:
-							if x_min == -1 or x < x_min:
-								x_min = x
-							if x >= x_max:
-								x_max = x + 1
+			x_min, y_min, x_max, y_max = self.images_bounds[z]
+			# x_min, x_max, y_min, y_max = (-1,)*4
+			# for y,line in enumerate(frame):
+			# 	if line != [self.transindex] * len(line):
+			# 		if y_min == -1:
+			# 			y_min = y
+			# 		y_max = y + 1
+			# 		for x,index in enumerate(line):
+			# 			if index != self.transindex:
+			# 				if x_min == -1 or x < x_min:
+			# 					x_min = x
+			# 				if x >= x_max:
+			# 					x_max = x + 1
 			if uncompressed:
 				data = ''.join(''.join(chr(i) for i in l[x_min:x_max]) for l in frame[y_min:y_max])
 				if data in frame_history:

Libs/DialogBIN.py
@@ -243,9 +243,122 @@ SCREEN_INFO = {
 	# 	"name":""
 	# },
 }
-DIALOG_ASSETS = {
-	
-}
+
+# DIALOG_ASSET's are frame indexes into dlg.grp
+DIALOG_ASSET_RADIO_DISABLED = 6
+DIALOG_ASSET_RADIO_UNSELECTED = 7
+DIALOG_ASSET_RADIO_UNSELECTED_CLICKED = 8
+DIALOG_ASSET_RADIO_SELECTED = 9
+DIALOG_ASSET_RADIO_SELECTED_CLICKED = 10
+
+DIALOG_ASSET_CHECK_DISABLED = 11
+DIALOG_ASSET_CHECK_UNSELECTED = 12
+DIALOG_ASSET_CHECK_UNSELECTED_CLICKED = 13
+DIALOG_ASSET_CHECK_SELECTED = 14
+DIALOG_ASSET_CHECK_SELECTED_CLICKED = 15
+
+DIALOG_ASSET_SCROLL_UP_DISABLED = 16
+DIALOG_ASSET_SCROLL_UP = 17
+DIALOG_ASSET_SCROLL_UP_CLICKED = 18
+DIALOG_ASSET_SCROLL_DOWN_DISABLED = 19
+DIALOG_ASSET_SCROLL_DOWN = 20
+DIALOG_ASSET_SCROLL_DOWN_CLICKED = 21
+DIALOG_ASSET_SCROLL_LEFT_DISABLED = 22
+DIALOG_ASSET_SCROLL_LEFT = 23
+DIALOG_ASSET_SCROLL_LEFT_CLICKED = 24
+DIALOG_ASSET_SCROLL_RIGHT_DISABLED = 25
+DIALOG_ASSET_SCROLL_RIGHT = 26
+DIALOG_ASSET_SCROLL_RIGHT_CLICKED = 27
+
+DIALOG_ASSET_SCROLL_BAR = 28
+
+DIALOG_ASSET_SCROLL_VERTICAL_TOP = 29
+DIALOG_ASSET_SCROLL_VERTICAL_MIDDLE = 30
+DIALOG_ASSET_SCROLL_VERTICAL_BOTTOM = 31
+DIALOG_ASSET_SCROLL_HORIZONTAL_LEFT = 32
+DIALOG_ASSET_SCROLL_HORIZONTAL_MIDDLE = 33
+DIALOG_ASSET_SCROLL_HORIZONTAL_RIGHT = 34
+
+DIALOG_ASSET_COMBO_DROP_TL = 35
+DIALOG_ASSET_COMBO_DROP_T = 36
+DIALOG_ASSET_COMBO_DROP_TR = 37
+DIALOG_ASSET_COMBO_DROP_L = 38
+DIALOG_ASSET_COMBO_DROP_M = 39
+DIALOG_ASSET_COMBO_DROP_R = 40
+DIALOG_ASSET_COMBO_DROP_BL = 41
+DIALOG_ASSET_COMBO_DROP_B = 42
+DIALOG_ASSET_COMBO_DROP_BR = 43
+DIALOG_ASSET_COMBO_LEFT_DROPPED_UP = 44
+DIALOG_ASSET_COMBO_MIDDLE_DROPPED_UP = 45
+DIALOG_ASSET_COMBO_RIGHT_DROPPED_UP = 46
+DIALOG_ASSET_COMBO_LEFT_DROPPED_DOWN = 47
+DIALOG_ASSET_COMBO_MIDDLE_DROPPED_DOWN = 48
+DIALOG_ASSET_COMBO_RIGHT_DROPPED_DOWN = 49
+
+DIALOG_ASSET_COMBO_ARROW = 50
+DIALOG_ASSET_COMBO_ARROW_HOVER = 51
+DIALOG_ASSET_COMBO_ARROW_DISABLED = 52
+
+DIALOG_ASSET_COMBO_LEFT = 53
+DIALOG_ASSET_COMBO_MIDDLE = 54
+DIALOG_ASSET_COMBO_RIGHT = 55
+DIALOG_ASSET_COMBO_LEFT_HOVER = 56
+DIALOG_ASSET_COMBO_MIDDLE_HOVER = 57
+DIALOG_ASSET_COMBO_RIGHT_HOVER = 58
+
+DIALOG_ASSET_SLIDER_LEFT_DISABLED = 91
+DIALOG_ASSET_SLIDER_MIDDLE_DISABLED = 92
+DIALOG_ASSET_SLIDER_RIGHT_DISABLED = 93
+DIALOG_ASSET_SLIDER_LEFT = 94
+DIALOG_ASSET_SLIDER_MIDDLE = 95
+DIALOG_ASSET_SLIDER_RIGHT = 96
+DIALOG_ASSET_SLIDER_SPOT_DISABLED = 97
+DIALOG_ASSET_SLIDER_SPOT = 98
+DIALOG_ASSET_SLIDER_DOT_DISABLED = 99
+DIALOG_ASSET_SLIDER_DOT_YELLOW = 100
+DIALOG_ASSET_SLIDER_DOT_GREEN = 101
+DIALOG_ASSET_SLIDER_DOT_RED = 102
+
+DIALOG_ASSET_BUTTON_LEFT_DISABLED_LEFT = 103
+DIALOG_ASSET_BUTTON_LEFT_DISABLED_MIDDLE = 104
+DIALOG_ASSET_BUTTON_LEFT_DISABLED_RIGHT = 105
+DIALOG_ASSET_BUTTON_LEFT_LEFT = 106
+DIALOG_ASSET_BUTTON_LEFT_MIDDLE = 107
+DIALOG_ASSET_BUTTON_LEFT_RIGHT = 108
+DIALOG_ASSET_BUTTON_LEFT_CLICKED_LEFT = 109
+DIALOG_ASSET_BUTTON_LEFT_CLICKED_MIDDLE = 110
+DIALOG_ASSET_BUTTON_LEFT_CLICKED_RIGHT = 111
+
+DIALOG_ASSET_BUTTON_MID_DISABLED_LEFT = 112
+DIALOG_ASSET_BUTTON_MID_DISABLED_MIDDLE = 113
+DIALOG_ASSET_BUTTON_MID_DISABLED_RIGHT = 114
+DIALOG_ASSET_BUTTON_MID_LEFT = 115
+DIALOG_ASSET_BUTTON_MID_MIDDLE = 116
+DIALOG_ASSET_BUTTON_MID_RIGHT = 117
+DIALOG_ASSET_BUTTON_MID_CLICKED_LEFT = 118
+DIALOG_ASSET_BUTTON_MID_CLICKED_MIDDLE = 119
+DIALOG_ASSET_BUTTON_MID_CLICKED_RIGHT = 120
+
+DIALOG_ASSET_BUTTON_RIGHT_DISABLED_LEFT = 121
+DIALOG_ASSET_BUTTON_RIGHT_DISABLED_MIDDLE = 122
+DIALOG_ASSET_BUTTON_RIGHT_DISABLED_RIGHT = 123
+DIALOG_ASSET_BUTTON_RIGHT_LEFT = 124
+DIALOG_ASSET_BUTTON_RIGHT_MIDDLE = 125
+DIALOG_ASSET_BUTTON_RIGHT_RIGHT = 126
+DIALOG_ASSET_BUTTON_RIGHT_CLICKED_LEFT = 127
+DIALOG_ASSET_BUTTON_RIGHT_CLICKED_MIDDLE = 128
+DIALOG_ASSET_BUTTON_RIGHT_CLICKED_RIGHT = 129
+
+# DIALOG_FRAMES's are frame indexes into tile.grp
+DIALOG_FRAME_TL = 0
+DIALOG_FRAME_T = 1
+DIALOG_FRAME_TR = 2
+DIALOG_FRAME_L = 3
+DIALOG_FRAME_M = 4
+DIALOG_FRAME_R = 5
+DIALOG_FRAME_BL = 6
+DIALOG_FRAME_B = 7
+DIALOG_FRAME_BR = 8
 
 class BINWidget(object):
 	BYTE_SIZE = 86
@@ -345,12 +458,6 @@ class BINWidget(object):
 		x2 = (self.x2 if self.x2 > self.x1 else self.x1)
 		y2 = (self.y2 if self.y2 > self.y1 else self.y1)
 		return [x1,y1,x2,y2]
-
-	def text_box(self):
-		box = self.bounding_box()
-		box[0] += self.text_offset_x
-		box[1] += self.text_offset_y
-		return box
 
 	def has_responsive(self):
 		return (self.flags & BINWidget.FLAG_RESPONSIVE == BINWidget.FLAG_RESPONSIVE) #(self.responsive_x1 or self.responsive_y1 or self.responsive_x2 or self.responsive_y2)

Libs/FNT.py
@@ -74,7 +74,6 @@ def letter_to_photo(palette, letter, color, remap=None, remap_palette=None):
 	color_map = remap[color]
 	while isinstance(color_map, int):
 		color_map = remap[color_map]
-	print (color,color_map)
 	data = []
 	for y in letter:
 		data.extend(y)

Libs/setutils.py
@@ -60,7 +60,7 @@ def loadsize(window, settings, setting, full=False):
 			y = 0
 		if h > screen_h and resizable[1] and screen_h > min_size[1]:
 			h = screen_h
-		window.geometry('%dx%d%+d%+d' % (w,h, x,y))
+		window.geometry('%dx%d+%d+%d' % (w,h, x,y))
 		window.update_idletasks()
 		if fullscreen:
 			try:

Libs/utils.py
@@ -25,7 +25,7 @@ couriersmall = ('Courier', -8, 'normal')
 ARROW = None
 
 def parse_geometry(geometry):
-	match = re.match('(\\d+)x(\\d+)([+-]\\d+)([+-]\\d+)(^)?',geometry)
+	match = re.match(r'(\d+)x(\d+)\+(-?\d+)\+(-?\d+)(\^?)',geometry)
 	return tuple(int(v) for v in match.groups()[:-1]) + (True if match.group(5) else False,)
 
 def isstr(s):

commit 911500f38d5d492e1c8ad038bec8202e4ca342da

Fix PyGRP frame importing/deleting which was broken by 7b8421bb
(Also fixes image_bounds which had never worked)
Better solution would be to have GRP.add_frames() and delete_frames()
methods or something.

Libs/GRP.py
@@ -36,9 +36,10 @@ def rle_outline(pal, index, ally_status=OUTLINE_SELF):
 	return (rgb[0],rgb[1],rgb[2], 255)
 
 def image_bounds(image, transindex=0):
+	width = len(image[0])
 	bounds = [-1,-1,-1,-1]
-	for y,yd in enumerate(d):
-		if yd.count(transindex) != g.width:
+	for y,yd in enumerate(image):
+		if yd.count(transindex) != width:
 			if bounds[1] == -1:
 				bounds[1] = y
 			bounds[3] = y

PyGRP.pyw
@@ -762,6 +762,7 @@ class PyGRP(Tk):
 					self.grp.height = fs.height
 				sel = self.listbox.size()
 				self.grp.images.extend(fs.images)
+				self.grp.images_bounds += [GRP.image_bounds(img) for img in fs.images]
 				for i in fs.images:
 					self.frames.append({})
 					self.listbox.insert(END, '%sFrame %s' % ('   ' * (frame / 17 % 2), frame))
@@ -788,6 +789,7 @@ class PyGRP(Tk):
 			i -= 1
 		for n,index in enumerate(indexs):
 			del self.grp.images[index-n]
+			del self.grp.images_bounds[index-n]
 			del self.frames[index-n]
 		self.grp.frames = len(self.grp.images)
 		if not self.grp.frames: