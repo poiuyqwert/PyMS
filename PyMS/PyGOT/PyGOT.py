26327c2f8dbd4c33dc78b0cdc3c45b30764b0f82
Zach Zahos <zzahos@konradgroup.com>
2021-06-11 15:43:41 -0600

    PyGOT refactored

--- PyMS/PyGOT/PyGOT.py
+++ PyMS/PyGOT/PyGOT.py
@@ -1,15 +1,24 @@
-from Libs.utils import *
-from Libs.setutils import *
-from Libs.trace import setup_trace
-from Libs import TRG, GOT
-from Libs.analytics import *
 
-from Tkinter import *
-from tkMessageBox import *
-import tkFileDialog,tkColorChooser
+from ..FileFormats.GOT import GOT
+from ..FileFormats.TRG import TRG
 
-from thread import start_new_thread
-import optparse, os, webbrowser, sys
+from ..Utilities.utils import BASE_DIR, VERSIONS, WIN_REG_AVAILABLE, fit, register_registry, couriernew
+from ..Utilities.UIKit import *
+from ..Utilities.Tooltip import Tooltip
+from ..Utilities.Settings import Settings
+from ..Utilities.analytics import ga, GAScreen
+from ..Utilities.trace import setup_trace
+from ..Utilities.Toolbar import Toolbar
+from ..Utilities.IntegerVar import IntegerVar
+from ..Utilities.SStringVar import SStringVar
+from ..Utilities.DropDown import DropDown
+from ..Utilities.StatusBar import StatusBar
+from ..Utilities.UpdateDialog import UpdateDialog
+from ..Utilities.PyMSError import PyMSError
+from ..Utilities.ErrorDialog import ErrorDialog
+from ..Utilities.AboutDialog import AboutDialog
+
+import os, webbrowser
 
 LONG_VERSION = 'v%s' % VERSIONS['PyGOT']
 
@@ -26,23 +35,18 @@ def tip(o, h):
 def get_info(label):
 	if label == 'TeamMode':
 		return ['Off','2 Teams','3 Teams','4 Teams']
-	if label in GOT.GOT.labels:
-		return GOT.GOT.info[GOT.GOT.labels.index(label)]
+	if label in GOT.labels:
+		return GOT.info[GOT.labels.index(label)]
 	return None
 
-class PyGOT(Tk):
+class PyGOT(MainWindow):
 	def __init__(self, guifile=None):
-		self.settings = loadsettings('PyGOT')
+		self.settings = Settings('PyGOT', '1')
 
 		#Window
-		Tk.__init__(self)
+		MainWindow.__init__(self)
 		self.title('PyGOT %s' % LONG_VERSION)
-		try:
-			self.icon = os.path.join(BASE_DIR,'Images','PyGOT.ico')
-			self.wm_iconbitmap(self.icon)
-		except:
-			self.icon = '@%s' % os.path.join(BASE_DIR, 'Images','PyGOT.xbm')
-			self.wm_iconbitmap(self.icon)
+		self.set_icon('PyGOT')
 		self.protocol('WM_DELETE_WINDOW', self.exit)
 		ga.set_application('PyGOT', VERSIONS['PyGOT'])
 		ga.track(GAScreen('PyGOT'))
@@ -54,46 +58,27 @@ class PyGOT(Tk):
 		self.edited = False
 
 		#Toolbar
-		buttons = [
-			('new', self.new, 'New (Ctrl+N)', NORMAL, 'Ctrl+N'),
-			2,
-			('open', self.open, 'Open (Ctrl+O)', NORMAL, 'Ctrl+O'),
-			('import', self.iimport, 'Import Game Template (Ctrl+I)', NORMAL, 'Ctrl+I'),
-			2,
-			('save', self.save, 'Save (Ctrl+S)', DISABLED, 'Ctrl+S'),
-			('saveas', self.saveas, 'Save As (Ctrl+Alt+A)', DISABLED, 'Ctrl+Alt+A'),
-			('export', self.export, 'Export Game Template (Ctrl+E)', DISABLED, 'Ctrl+E'),
-			2,
-			('close', self.close, 'Close (Ctrl+W)', DISABLED, 'Ctrl+W'),
-			10,
-			('codeedit', lambda e=None,t=True: self.trg(e,t), 'Convert *.trg to GOT compatable (Ctrl+T)', NORMAL, 'Ctrl+T'),
-			('insert', lambda e=None,t=False: self.trg(e,t), 'Revert GOT compatable *.trg (Ctrl+Alt+T)', NORMAL, 'Ctrl+Alt+T'),
-			10,
-			('register', self.register, 'Set as default *.got editor (Windows Only)', [DISABLED,NORMAL][win_reg], ''),
-			('help', self.help, 'Help (F1)', NORMAL, 'F1'),
-			('about', self.about, 'About PyGOT', NORMAL, ''),
-			10,
-			('exit', self.exit, 'Exit (Alt+F4)', NORMAL, 'Alt+F4'),
-		]
-		self.buttons = {}
-		toolbar = Frame(self)
-		for btn in buttons:
-			if isinstance(btn, tuple):
-				image = PhotoImage(file=os.path.join(BASE_DIR,'Images','%s.gif' % btn[0]))
-				button = Button(toolbar, image=image, width=20, height=20, command=btn[1], state=btn[3])
-				button.image = image
-				button.tooltip = Tooltip(button, btn[2])
-				button.pack(side=LEFT)
-				self.buttons[btn[0]] = button
-				a = btn[4]
-				if a:
-					if not a.startswith('F'):
-						self.bind('<%s%s>' % (a[:-1].replace('Ctrl','Control').replace('+','-'), a[-1].lower()), btn[1])
-					else:
-						self.bind('<%s>' % a, btn[1])
-			else:
-				Frame(toolbar, width=btn).pack(side=LEFT)
-		toolbar.pack(side=TOP, padx=1, pady=1, fill=X)
+		self.toolbar = Toolbar(self)
+		self.toolbar.add_button('new', self.new, 'New', Ctrl.n)
+		self.toolbar.add_gap()
+		self.toolbar.add_button('open', self.open, 'Open', Ctrl.o)
+		self.toolbar.add_button('import', self.iimport, 'Import Game Template', Ctrl.i)
+		self.toolbar.add_gap()
+		self.toolbar.add_button('save', self.save, 'Save', Ctrl.s, enabled=False, tags='file_open')
+		self.toolbar.add_button('saveas', self.saveas, 'Save As', Ctrl.Alt.a, enabled=False, tags='file_open')
+		self.toolbar.add_button('export', self.export, 'Export Game Template', Ctrl.e, enabled=False, tags='file_open')
+		self.toolbar.add_gap()
+		self.toolbar.add_button('close', self.close, 'Close', Ctrl.w, enabled=False, tags='file_open')
+		self.toolbar.add_section()
+		self.toolbar.add_button('codeedit', lambda: self.trg(True), 'Convert *.trg to GOT compatable', Ctrl.t)
+		self.toolbar.add_button('insert', lambda: self.trg(False), 'Revert GOT compatable *.trg', Ctrl.Alt.t)
+		self.toolbar.add_section()
+		self.toolbar.add_button('register', self.register, 'Set as default *.got editor (Windows Only)', enabled=WIN_REG_AVAILABLE)
+		self.toolbar.add_button('help', self.help, 'Help', Key.F1)
+		self.toolbar.add_button('about', self.about, 'About PyGOT')
+		self.toolbar.add_section()
+		self.toolbar.add_button('exit', self.exit, 'Exit', Alt.F4)
+		self.toolbar.pack(side=TOP, padx=1, pady=1, fill=X)
 
 		self.input = {}
 
@@ -174,14 +159,11 @@ class PyGOT(Tk):
 
 		#Statusbar
 		self.status = StringVar()
-		statusbar = Frame(self)
-		Label(statusbar, textvariable=self.status, bd=1, relief=SUNKEN, width=35, anchor=W).pack(side=LEFT, padx=1)
-		image = PhotoImage(file=os.path.join(BASE_DIR,'Images','save.gif'))
-		self.editstatus = Label(statusbar, image=image, bd=0, state=DISABLED)
-		self.editstatus.image = image
-		self.editstatus.pack(side=LEFT, padx=1, fill=Y)
-		Label(statusbar, bd=1, relief=SUNKEN, anchor=W).pack(side=LEFT, expand=1, padx=1, fill=X)
 		self.status.set('Load or create a Game Template.')
+		statusbar = StatusBar(self)
+		statusbar.add_label(self.status, width=35)
+		self.editstatus = statusbar.add_icon(PhotoImage(file=os.path.join(BASE_DIR, 'PyMS', 'Images', 'save.gif')))
+		statusbar.add_spacer()
 		statusbar.pack(side=BOTTOM, fill=X)
 
 		self.values = [
@@ -205,40 +187,43 @@ class PyGOT(Tk):
 			self.resourcevalue,
 		]
 
+		self.settings.windows.load_window_size('main', self)
+
 		if guifile:
 			self.open(file=guifile)
 
-		start_new_thread(check_update, (self, 'PyGOT'))
+		UpdateDialog.check_update(self, 'PyGOT')
 
 	def unsaved(self):
 		if self.got and self.edited:
 			file = self.file
 			if not file:
 				file = 'Unnamed.got'
-			save = askquestion(parent=self, title='Save Changes?', message="Save changes to '%s'?" % file, default=YES, type=YESNOCANCEL)
-			if save != 'no':
-				if save == 'cancel':
+			save = MessageBox.askquestion(parent=self, title='Save Changes?', message="Save changes to '%s'?" % file, default=MessageBox.YES, type=MessageBox.YESNOCANCEL)
+			if save != MessageBox.NO:
+				if save == MessageBox.CANCEL:
 					return True
 				if self.file:
 					self.save()
 				else:
 					self.saveas()
 
-	def select_file(self, title, open=True, ext='.got', filetypes=[('StarCraft Game Templates','*.got'),('All Files','*')]):
-		path = self.settings.get('lastpath', BASE_DIR)
-		self._pyms__window_blocking = True
-		file = [tkFileDialog.asksaveasfilename,tkFileDialog.askopenfilename][open](parent=self, title=title, defaultextension=ext, filetypes=filetypes, initialdir=path)
-		self._pyms__window_blocking = False
-		if file:
-			self.settings['lastpath'] = os.path.dirname(file)
-		return file
+	# def select_file(self, title, open=True, ext='.got', filetypes=[('StarCraft Game Templates','*.got'),('All Files','*')]):
+	# 	path = self.settings.get('lastpath', BASE_DIR)
+	# 	self._pyms__window_blocking = True
+	# 	file = [tkFileDialog.asksaveasfilename,tkFileDialog.askopenfilename][open](parent=self, title=title, defaultextension=ext, filetypes=filetypes, initialdir=path)
+	# 	self._pyms__window_blocking = False
+	# 	if file:
+	# 		self.settings['lastpath'] = os.path.dirname(file)
+	# 	return file
+
+	def is_file_open(self):
+		return not not self.got
 
 	def action_states(self):
-		file = [NORMAL,DISABLED][not self.got]
-		for btn in ['save','saveas','export','close']:
-			self.buttons[btn]['state'] = file
+		self.toolbar.tag_enabled('file_open', self.is_file_open())
 		for inp in ['name','id','sublabel','subvalue','subid','victory','resource','stats','fog','units','start','players','allies','teams','cheats','tourny']:
-			self.input[inp]['state'] = file
+			self.input[inp]['state'] = self.is_file_open()
 		if self.got:
 			self.input['victoryvalue']['state'] = [DISABLED,NORMAL][self.victory.get() > 0 and not self.victory.get() % 3]
 			self.input['resourcevalue']['state'] = [DISABLED,NORMAL][self.resource.get() % 4 == 1]
@@ -279,7 +264,7 @@ class PyGOT(Tk):
 
 	def new(self, key=None):
 		if not self.unsaved():
-			self.got = GOT.GOT()
+			self.got = GOT()
 			self.file = None
 			self.status.set('Editing new Game Template.')
 			self.title('PyGOT %s (Unnamed.got)' % LONG_VERSION)
@@ -291,10 +276,10 @@ class PyGOT(Tk):
 	def open(self, key=None, file=None):
 		if not self.unsaved():
 			if file == None:
-				file = self.select_file('Open GOT')
+				file = self.settings.lastpath.got.select_file('open', self, 'Open GOT', '.got', [('StarCraft Game Templates','*.got'),('All Files','*')])
 				if not file:
 					return
-			got = GOT.GOT()
+			got = GOT()
 			try:
 				got.load_file(file)
 			except PyMSError, e:
@@ -314,10 +299,10 @@ class PyGOT(Tk):
 
 	def iimport(self, key=None):
 		if not self.unsaved():
-			file = self.select_file('Import TXT', True, '*.txt', [('Text Files','*.txt'),('All Files','*')])
+			file = self.settings.lastpath.txt.select_file('import', self, 'Import TXT', '.txt', [('Text Files','*.txt'),('All Files','*')])
 			if not file:
 				return
-			got = GOT.GOT()
+			got = GOT()
 			try:
 				got.interpret(file)
 			except PyMSError, e:
@@ -357,7 +342,7 @@ class PyGOT(Tk):
 	def saveas(self, key=None):
 		if key and self.buttons['saveas']['state'] != NORMAL:
 			return
-		file = self.select_file('Save GOT As', False)
+		file = self.settings.lastpath.got.select_file('save', self, 'Save GOT As', '.got', [('StarCraft Game Templates','*.got'),('All Files','*')], save=True)
 		if not file:
 			return True
 		self.file = file
@@ -366,7 +351,7 @@ class PyGOT(Tk):
 	def export(self, key=None):
 		if key and self.buttons['export']['state'] != NORMAL:
 			return
-		file = self.select_file('Export TXT', False, '*.txt', [('Text Files','*.txt'),('All Files','*')])
+		file = self.settings.lastpath.txt.select_file('export', self, 'Export TXT', '.txt', [('Text Files','*.txt'),('All Files','*')], save=True)
 		if not file:
 			return True
 		try:
@@ -389,16 +374,16 @@ class PyGOT(Tk):
 			self.action_states()
 
 	def trg(self, e=None, t=0):
-		file = self.select_file('Open TRG', True, '*.trg', [('StarCraft Triggers','*.trg'),('All Files','*')])
+		file = self.settings.lastpath.trg.select_file('open', self, 'Open TRG', '.trg', [('StarCraft Triggers','*.trg'),('All Files','*')])
 		if not file:
 			return
-		trg = TRG.TRG()
+		trg = TRG()
 		try:
-			trg.load_file(trig)
+			trg.load_file(file)
 		except PyMSError, e:
 			ErrorDialog(self, e)
 			return
-		file = self.select_file('Save TRG', False, '*.trg', [('StarCraft Triggers','*.trg'),('All Files','*')])
+		file = self.settings.lastpath.trg.select_file('save', self, 'Save TRG', '.trgg', [('StarCraft Triggers','*.trg'),('All Files','*')])
 		if not file:
 			return True
 		try:
@@ -408,7 +393,7 @@ class PyGOT(Tk):
 
 	def register(self, e=None):
 		try:
-			register_registry('PyGOT','','got',os.path.join(BASE_DIR, 'PyGOT.pyw'),os.path.join(BASE_DIR,'Images','PyGOT.ico'))
+			register_registry('PyGOT','','got',os.path.join(BASE_DIR, 'PyGOT.pyw'),os.path.join(BASE_DIR, 'PyMS', 'Images', 'PyGOT.ico'))
 		except PyMSError, e:
 			ErrorDialog(self, e)
 
@@ -420,65 +405,6 @@ class PyGOT(Tk):
 
 	def exit(self, e=None):
 		if not self.unsaved():
-			savesettings('PyGOT', self.settings)
+			self.settings.windows.save_window_size('main', self)
+			self.settings.save()
 			self.destroy()
-
-def main():
-	import sys
-	if not sys.argv or (len(sys.argv) == 1 and os.path.basename(sys.argv[0]).lower() in ['','pygot.py','pygot.pyw','pygot.exe']):
-		gui = PyGOT()
-		gui.startup()
-	else:
-		p = optparse.OptionParser(usage='usage: PyGOT [options] <inp> [out]', version='PyGOT %s' % LONG_VERSION)
-		p.add_option('-d', '--decompile', action='store_true', dest='convert', help="Decompile a GOT file [default]", default=True)
-		p.add_option('-c', '--compile', action='store_false', dest='convert', help="Compile a GOT file")
-		p.add_option('-t', '--trig', help="Used to compile a TRG file to a GOT compatable TRG file")
-		p.add_option('-r', '--reference', action='store_true', help="When decompiling, put a reference for settings at the top of the file [default: Off]", default=False)
-		p.add_option('--gui', help="Opens a file with the GUI", default='')
-		opt, args = p.parse_args()
-		if opt.gui:
-			gui = PyGOT(opt.gui)
-			gui.startup()
-		else:
-			if not len(args) in [1,2]:
-				p.error('Invalid amount of arguments')
-			path = os.path.dirname(args[0])
-			if not path:
-				path = os.path.abspath('')
-			got = GOT.GOT()
-			if len(args) == 1:
-				if opt.convert:
-					ext = 'txt'
-				else:
-					ext = 'got'
-				args.append('%s%s%s' % (os.path.join(path,os.extsep.join(os.path.basename(args[0]).split(os.extsep)[:-1])), os.extsep, ext))
-			try:
-				if opt.convert:
-					print "Reading GOT '%s'..." % args[0]
-					got.load_file(args[0])
-					print " - '%s' read successfully\nDecompiling GOT file '%s'..." % (args[0],args[0])
-					got.decompile(args[1], opt.reference)
-					print " - '%s' written succesfully" % args[1]
-				else:
-					print "Interpreting file '%s'..." % args[0]
-					got.interpret(args[0])
-					print " - '%s' read successfully\nCompiling file '%s' to GOT format..." % (args[0],args[0])
-					lo.compile(args[1])
-					print " - '%s' written succesfully" % args[1]
-					if opt.trig:
-						print "Reading TRG '%s'..." % args[0]
-						trg = TRG.TRG()
-						trg.load_file(opt.trig)
-						print " - '%s' read successfully" % args[0]
-						path = os.path.dirname(opt.trig)
-						if not path:
-							path = os.path.abspath('')
-						file = '%s%s%s' % (os.path.join(path,os.extsep.join(os.path.basename(args[1]).split(os.extsep)[:-1])), os.extsep, 'trg')
-						print "Compiling file '%s' to GOT compatable TRG..." % file
-						trg.compile(file, True)
-						print " - '%s' written succesfully" % file
-			except PyMSError, e:
-				print repr(e)
-
-if __name__ == '__main__':
-	main()
