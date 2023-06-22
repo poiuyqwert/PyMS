510ad074646759789cf8dc43504b9a1c39302f58
poiuy_qwert <p.q.poiuy.qwert@gmail.com>
2023-06-22 11:26:21 -0600

    - Updated PyPCX

--- PyMS/PyPCX/PyPCX.py
+++ PyMS/PyPCX/PyPCX.py
@@ -17,11 +17,14 @@ from ..Utilities.AboutDialog import AboutDialog
 from ..Utilities.HelpDialog import HelpDialog
 from ..Utilities.fileutils import check_allow_overwrite_internal_file
 from ..Utilities.SettingsDialog import SettingsDialog
+from ..Utilities.CheckSaved import CheckSaved
+
+from typing import cast
 
 LONG_VERSION = 'v%s' % Assets.version('PyPCX')
 
 class PyPCX(MainWindow):
-	def __init__(self, guifile=None):
+	def __init__(self, guifile: str | None = None) -> None:
 		self.settings = Settings('PyPCX', '1')
 
 		#Window
@@ -33,8 +36,9 @@ class PyPCX(MainWindow):
 		setup_trace('PyPCX', self)
 		Theme.load_theme(self.settings.get('theme'), self)
 
-		self.pcx = None
-		self.file = None
+		self.pcx: PCX | None = None
+		self.file: str | None = None
+		self.image: Image | None = None
 		self.edited = False
 
 		self.update_title()
@@ -42,8 +46,12 @@ class PyPCX(MainWindow):
 		#Toolbar
 		self.toolbar = Toolbar(self)
 		self.toolbar.add_button(Assets.get_image('open'), self.open, 'Open', Ctrl.o)
-		self.toolbar.add_button(Assets.get_image('save'), self.save, 'Save', Ctrl.s, enabled=False, tags='file_open')
-		self.toolbar.add_button(Assets.get_image('saveas'), self.saveas, 'Save As', Ctrl.Alt.a, enabled=False, tags='file_open')
+		def save():
+			self.save()
+		self.toolbar.add_button(Assets.get_image('save'), save, 'Save', Ctrl.s, enabled=False, tags='file_open')
+		def saveas():
+			self.saveas()
+		self.toolbar.add_button(Assets.get_image('saveas'), saveas, 'Save As', Ctrl.Alt.a, enabled=False, tags='file_open')
 		self.toolbar.add_button(Assets.get_image('close'), self.close, 'Close', Ctrl.w, enabled=False, tags='file_open')
 		self.toolbar.add_gap()
 		self.toolbar.add_button(Assets.get_image('exportc'), self.export, 'Export as BMP', Ctrl.e, enabled=False, tags='file_open')
@@ -58,7 +66,7 @@ class PyPCX(MainWindow):
 		self.toolbar.add_section()
 		self.toolbar.add_button(Assets.get_image('asc3topyai'), self.sets, "Manage Settings", Ctrl.m)
 		self.toolbar.add_section()
-		self.toolbar.add_button(Assets.get_image('register'), self.register, 'Set as default *.pcx editor (Windows Only)', enabled=WIN_REG_AVAILABLE)
+		self.toolbar.add_button(Assets.get_image('register'), self.register_registry, 'Set as default *.pcx editor (Windows Only)', enabled=WIN_REG_AVAILABLE)
 		self.toolbar.add_button(Assets.get_image('help'), self.help, 'Help', Key.F1)
 		self.toolbar.add_button(Assets.get_image('about'), self.about, 'About PyPCX')
 		self.toolbar.add_section()
@@ -80,42 +88,46 @@ class PyPCX(MainWindow):
 		self.editstatus = statusbar.add_icon(Assets.get_image('save'))
 		statusbar.add_spacer()
 
-		self.settings.windows.load_window_size('main', self)
+		self.settings.windows.load_window_size('main', self, default_size=Size(600, 400))
 
 		if guifile:
 			self.open(file=guifile)
 
 		UpdateDialog.check_update(self, 'PyPCX')
 
-	def is_file_open(self):
+	def is_file_open(self) -> bool:
 		return not not self.pcx
 
-	def action_states(self):
+	def action_states(self) -> None:
 		self.toolbar.tag_enabled('file_open', self.is_file_open())
 		self.editstatus['state'] = NORMAL if self.edited else DISABLED
 
-	def preview(self):
+	def preview(self) -> None:
+		if not self.pcx:
+			return
 		self.canvas.config(width=self.pcx.width,height=self.pcx.height)
 		self.canvas.pack(side=TOP)
-		self.canvas.pcx = frame_to_photo(self.pcx.palette, self.pcx, -1, size=False, trans=False)
-		self.canvas.create_image(0, 0, image=self.canvas.pcx, anchor=NW)
+		self.image = cast(Image, frame_to_photo(self.pcx.palette, self.pcx, -1, size=False, trans=False))
+		self.canvas.create_image(0, 0, image=self.image, anchor=NW)
 		self.action_states()
 
-	def unsaved(self):
-		if self.pcx and self.edited:
-			file = self.file
-			if not file:
-				file = 'Unnamed.pcx'
-			save = MessageBox.askquestion(parent=self, title='Save Changes?', message="Save changes to '%s'?" % file, default=MessageBox.YES, type=MessageBox.YESNOCANCEL)
-			if save != MessageBox.NO:
-				if save == MessageBox.CANCEL:
-					return True
-				if self.file:
-					self.save()
-				else:
-					self.saveas()
+	def check_saved(self) -> CheckSaved:
+		if not self.pcx or not self.edited:
+			return CheckSaved.saved
+		file = self.file
+		if not file:
+			file = 'Unnamed.pcx'
+		save = MessageBox.askquestion(parent=self, title='Save Changes?', message="Save changes to '%s'?" % file, default=MessageBox.YES, type=MessageBox.YESNOCANCEL)
+		if save == MessageBox.NO:
+			return CheckSaved.saved
+		elif save == MessageBox.CANCEL:
+			return CheckSaved.cancelled
+		if self.file:
+			return self.save()
+		else:
+			return self.saveas()
 
-	def update_title(self):
+	def update_title(self) -> None:
 		file_path = self.file
 		if not file_path and self.is_file_open():
 			file_path = 'Untitled.pcx'
@@ -124,8 +136,8 @@ class PyPCX(MainWindow):
 		else:
 			self.title('PyPCX %s (%s)' % (LONG_VERSION, file_path))
 
-	def open(self, key=None, file=None):
-		if self.unsaved():
+	def open(self, file: str | None = None) -> None:
+		if self.check_saved() == CheckSaved.cancelled:
 			return
 		if file is None:
 			file = self.settings.lastpath.pcx.select_open_file(self, title='Open PCX', filetypes=[FileType.pcx()])
@@ -144,33 +156,36 @@ class PyPCX(MainWindow):
 		self.status.set('Load Successful!')
 		self.preview()
 
-	def save(self, key=None):
-		self.saveas(file_path=self.file)
+	def save(self) -> CheckSaved:
+		return self.saveas(file_path=self.file)
 
-	def saveas(self, key=None, file_path=None):
+	def saveas(self, file_path: str | None = None) -> CheckSaved:
+		if not self.pcx:
+			return CheckSaved.saved
 		if not file_path:
 			file_path = self.settings.lastpath.pcx.select_save_file(self, title='Save PCX As', filetypes=[FileType.pcx()])
 			if not file_path:
-				return
+				return CheckSaved.cancelled
 		elif not check_allow_overwrite_internal_file(file_path):
-			return
+			return CheckSaved.cancelled
 		try:
 			self.pcx.save_file(file_path)
 		except PyMSError as e:
 			ErrorDialog(self, e)
-			return
+			return CheckSaved.cancelled
 		self.edited = False
 		self.status.set('Save Successful!')
 		self.action_states()
 		self.file = file_path
 		self.update_title()
+		return CheckSaved.saved
 
-	def loadpal(self, key=None):
-		if not self.is_file_open():
+	def loadpal(self) -> None:
+		if not self.pcx:
 			return
 		file = self.settings.lastpath.pal.select_open_file(self, key='import', title='Import Palette', filetypes=Palette.FileType.load_types())
 		if not file:
-			return True
+			return
 		pal = Palette()
 		try:
 			pal.load_file(file)
@@ -181,12 +196,12 @@ class PyPCX(MainWindow):
 		self.edited = True
 		self.preview()
 
-	def savepal(self, key=None, file_type=Palette.FileType.sc_pal):
-		if not self.is_file_open():
+	def savepal(self, file_type: Palette.FileType = Palette.FileType.sc_pal) -> None:
+		if not self.pcx:
 			return
 		file = self.settings.lastpath.pal.select_save_file(self, key='export', title='Save Palette As', filetypes=Palette.FileType.save_types(file_type.format, file_type.ext))
 		if not file:
-			return True
+			return
 		p = Palette()
 		p.palette = list(self.pcx.palette)
 		try:
@@ -195,12 +210,12 @@ class PyPCX(MainWindow):
 		except PyMSError as e:
 			ErrorDialog(self, e)
 
-	def export(self, key=None):
-		if not self.is_file_open():
+	def export(self) -> None:
+		if not self.pcx:
 			return
 		file = self.settings.lastpath.bmp.select_save_file(self, key='export', title='Export to BMP', filetypes=[FileType.bmp()])
 		if not file:
-			return True
+			return
 		b = BMP()
 		b.set_pixels(self.pcx.image,self.pcx.palette)
 		try:
@@ -210,12 +225,12 @@ class PyPCX(MainWindow):
 			return
 		self.status.set('Image exported successfully!')
 
-	def iimport(self, key=None):
-		if not self.is_file_open():
+	def iimport(self) -> None:
+		if self.check_saved() == CheckSaved.cancelled:
 			return
 		file = self.settings.lastpath.bmp.select_open_file(self, key='import', title='Import a BMP', filetypes=[FileType.bmp()])
 		if not file:
-			return True
+			return
 		b = BMP()
 		try:
 			b.load_file(file)
@@ -231,10 +246,8 @@ class PyPCX(MainWindow):
 		self.preview()
 		self.status.set('Image imported successfully!')
 
-	def close(self, key=None):
-		if self.unsaved():
-			return
-		if not self.is_file_open():
+	def close(self) -> None:
+		if self.check_saved() == CheckSaved.cancelled:
 			return
 		self.pcx = None
 		self.file = None
@@ -242,26 +255,26 @@ class PyPCX(MainWindow):
 		self.status.set('Load a PCX or import a BMP.')
 		self.canvas.delete(ALL)
 		self.canvas.forget()
-		self.canvas.pcx = None
+		self.image = None
 		self.action_states()
 
-	def register(self, e=None):
+	def register_registry(self) -> None:
 		try:
 			register_registry('PyPCX', 'pcx')
 		except PyMSError as e:
 			ErrorDialog(self, e)
 
-	def sets(self, key=None, err=None):
+	def sets(self, err: PyMSError | None = None) -> None:
 		SettingsDialog(self, [('Theme',)], (550,380), err, settings=self.settings)
 
-	def help(self, e=None):
+	def help(self) -> None:
 		HelpDialog(self, self.settings, 'Help/Programs/PyPCX.md')
 
-	def about(self, key=None):
+	def about(self) -> None:
 		AboutDialog(self, 'PyPCX', LONG_VERSION)
 
-	def exit(self, e=None):
-		if self.unsaved():
+	def exit(self) -> None:
+		if self.check_saved() == CheckSaved.cancelled:
 			return
 		self.settings.windows.save_window_size('main', self)
 		self.settings.save()
--- PyPCX.pyw
+++ PyPCX.pyw
@@ -3,7 +3,7 @@
 from PyMS.Utilities.Compatibility import check_compat, Requirement
 check_compat('PyPCX', Requirement.PIL)
 
-def main():
+def main() -> None:
 	from PyMS.PyPCX.PyPCX import PyPCX, LONG_VERSION
 
 	from PyMS.FileFormats import PCX
